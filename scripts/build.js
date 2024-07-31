47ccd0ef823cf24bf0114bc83c44e33a42ad1038
RanolP <me@ranolp.dev>
2024-08-01 03:51:39 +0900

    fix: website bug fixes
    - /unofficial-documentation as basepath
    - upgrade stoplight/elements
    - alter hybrids with solid-element

--- scripts/build.js
+++ scripts/build.js
@@ -3,11 +3,13 @@ import { $ } from 'zx';
 await $`tsp compile src`;
 await $`rm -rf build`;
 await $`mkdir build`;
-await $`cp favicon.svg build`;
-await $`cp index.html build`;
-await $`cp -r tsp-output build`;
-await $`cp -r vendors build`;
-await $`cp -r assets build`;
+await $`mkdir build/unofficial-documentation`;
+await $`cp favicon.svg build/unofficial-documentation`;
+await $`cp index.html build/unofficial-documentation`;
+await $`cp -r tsp-output build/unofficial-documentation`;
+await $`cp -r vendors build/unofficial-documentation`;
+await $`cp -r assets build/unofficial-documentation`;
+await $`echo '<meta http-equiv="refresh" content="0; url=/unofficial-documentation" />' >build/index.html`;
 
 const branch = await $`git branch --show-current`.quiet();
 if (branch.stdout.trim() != 'main') {
